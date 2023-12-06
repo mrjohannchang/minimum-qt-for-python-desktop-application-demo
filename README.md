9355951daa0516fbeaab2890ea127ed051545615
Johann Chang <johann.chang@outlook.com>
2023-12-06 09:07:49 -0300

    Demonstrate how to get the component from the Qt Designer UI file

--- README.md
+++ README.md
@@ -90,6 +90,8 @@ pdm install
 
 8. Demonstrate how to hook the UI building
 
+9. Demonstrate how to get the component from the Qt Designer UI file
+
 ## License
 
 [Mozilla Public License Version 2.0](https://www.mozilla.org/en-US/MPL/2.0/)
--- src/minimum_qt_for_python_desktop_application_demo_desktop/ui/main_window.py
+++ src/minimum_qt_for_python_desktop_application_demo_desktop/ui/main_window.py
@@ -1,6 +1,6 @@
 import minimum_qt_for_python_desktop_application_demo as sdk
 from PySide6.QtGui import QCloseEvent
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QPushButton
 
 
 class QMainWindowExt(QMainWindow):
@@ -11,3 +11,11 @@ class QMainWindowExt(QMainWindow):
     def set_up(self) -> None:
         sdk.get_logger().info("Setting up")
         self.setWindowTitle(f"Minimum Qt for Python Desktop Application v{sdk.VERSION}")
+
+        # The following line is optional. This is only for the IDE to recognize the push_button attribute.
+        self.push_button: QPushButton = getattr(self, 'push_button')
+
+        self.push_button.clicked.connect(self.on_push_button_clicked_listener)
+
+    def on_push_button_clicked_listener(self, checked: bool) -> None:
+        sdk.get_logger().info(f"Push button clicked, checked: {checked}")
